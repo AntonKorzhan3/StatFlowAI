82440623334247f9b10dcd5877deb93fb0bad42b
Anton Korzhan <antonk1200@gmail.com>
2024-05-12 20:10:34 +0100

    fixed typo

--- project/src/types/PveInfo.ts
+++ project/src/types/PveInfo.ts
@@ -2,5 +2,5 @@ export type PveInfo = {
   activitiesCleared: String | null;
   pveKills: String | null;
   pveDeaths: String | null;
-  weaponsBestType: String | null;
+  weaponBestType: String | null;
 };
